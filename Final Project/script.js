36e30a4f0b2be74c10e8d5e2733f9ce60edb70d9
mermatriarchy <hailey.miller91@gmail.com>
2017-05-30 18:48:24 -0400

    answer some of Gabi's final project questions

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,6 +1,10 @@
 
 $(document).ready(function(){
     $('.carousel').slick({
+      // you'll ned to set arrows: true here
+      // then you'll need to override some of the default styling that slick is putting on the page
+      // I would specifically look at the left: *some num of px* property and the z-index (think big numbers)
+      // to see if you can get them to show up
       accessibility:true,
       cssEase:'ease',
       dots: true,
@@ -38,6 +42,7 @@ $(document).ready(function(){
 });
 
 function myFunction() {
+  // What default behavior did you forget to prevent here?
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += "responsive";
